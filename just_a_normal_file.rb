02c49173978c0c03d405aa9522875f0c231717a0
Greg Ziegan <gziegan@zendesk.com>
2018-09-10 16:47:54 -0500

    Update just_a_normal_file.rb

--- just_a_normal_file.rb
+++ just_a_normal_file.rb
@@ -1,1 +1,287 @@
 # normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
+
+# normal stuff here
+def add(a, b)
+  a + b
+end
+
+# radme
