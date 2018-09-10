7640b9b69774d7de341ac4224b9a93f86f9f98ef
Greg Ziegan <gziegan@zendesk.com>
2018-09-10 11:19:28 -0500

    Update cool_file.rb

--- cool_file.rb
+++ cool_file.rb
@@ -8,11 +8,6 @@ class String
   def is_number?
     true if Float(self) rescue false
   end
-end
-class String
-  def is_number?
-    true if Float(self) rescue false
-  end
 endclass String
   def is_number?
     true if Float(self) rescue false
