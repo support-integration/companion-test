cad07c47598255b6774ceab8b6a28d8e7f5dff48
Greg Ziegan <greg.ziegan@gmail.com>
2018-10-10 15:43:48 -0500

    Update cool_file.rb

--- cool_file.rb
+++ cool_file.rb
@@ -268,3 +268,815 @@ class String
     true if Float(self) rescue false
   end
 end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+
+class String
+  def unowned?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+endclass String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+endclass String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+endclass String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+endclass String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+endclass String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+endclass String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+endclass String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+endclass String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+endclass String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+
+class String
+  def unowned?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+endclass String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+endclass String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+endclass String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+endclass String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+endclass String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+endclass String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+endclass String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+endclass String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+endclass String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+
+class String
+  def unowned?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+endclass String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+endclass String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+endclass String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+endclass String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+endclass String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+endclass String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+endclass String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+endclass String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+endclass String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
