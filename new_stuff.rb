f2c0851bee45a1252c80d2a5b3305eee96fe351a
Greg Ziegan <greg.ziegan@gmail.com>
2018-10-11 16:02:50 -0500

    Create new_stuff.rb

---
+++ new_stuff.rb
@@ -0,0 +1,399 @@
+# collected thoughts
+changes
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+# collected thoug# collected thoughts
+changes
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+hts
+
+
+changes
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+# collected thoug# collected thoughts
+changes
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+
+# collected thoughts
+changes
+# collected thoughts
+changes
+# collected thoughts
+changes
+hts
+
+
+changes
+# collected thoughts
+changes
