4229db0c6af0d19035207bb1b3c1ac58c983f725
Greg Ziegan <greg.ziegan@gmail.com>
2018-10-11 13:34:42 -0500

    Update collective.rb

--- collective.rb
+++ collective.rb
@@ -1,1 +1,282 @@
 # collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change
+# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change
+# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change
+# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change
+# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change
+# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change
+# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change
+# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change
+# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change
+# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change
+# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change
+# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change
+# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change
+# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change
+# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change
+# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change
+# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change
+# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change
+# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change
+# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change
+# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change
+# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change
+# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change
+# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change
+# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change
+# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change
+# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change
+# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change
+# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change
+# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change# collected thoughts
+large change
+large change
+large changelarge change
+large change
+large change
+large change
+large change
